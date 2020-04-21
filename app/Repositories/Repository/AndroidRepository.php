fc280f915d651e5ac4ceac453eac6d61d23bde8e
rahmadfirmansyah59@gmail.com <m4db4LL12345>
2020-04-21 15:07:16 +0700

    first commit

--- app/Repositories/Repository/AndroidRepository.php
+++ app/Repositories/Repository/AndroidRepository.php
@@ -8,6 +8,7 @@
 
 namespace App\Repositories\Repository;
 
+use App\Foto;
 use  App\User;
 use Illuminate\Support\Facades\DB;
 
@@ -19,26 +20,29 @@ class AndroidRepository
         $password = $data->password;
         $user = User::where('username',$username)->first();
 
+
+//        return decrypt($user->password);
+
         if (!empty($user)){
             if (decrypt($user->password) == $password){
 
                 $data = file_get_contents('http://cuti.sidoarjokab.go.id/api_login.php?NIP='.$user->username);
                 $pecah =  json_decode($data);
 
-                $back['error'] = 'false';
+                $back['error'] = false;
                 $back['code'] = 200;
                 $back['message'] = "Welocme";
                 $back['data'] = $pecah;
 
             }else{
-                $back['error'] = 'true';
+                $back['error'] = true;
                 $back['code'] = 422;
                 $back['message'] = "Password Salah";
 
 
             }
         }else{
-            $back['error'] = 'true';
+            $back['error'] = true;
             $back['code'] = 422;
             $back['message'] = "Usernmae Salah";
 
@@ -52,13 +56,13 @@ class AndroidRepository
 
         if ($data){
             $pecah = json_decode($data);
-            $back['error'] = 'false';
+            $back['error'] = false;
             $back['code'] = 200;
             $back['message'] = "Berhasil";
             $back['list'] = $pecah;
         }else
         {
-            $back['error'] = 'true';
+            $back['error'] = true;
             $back['code'] = 422;
             $back['message'] = "Data Kosong";
         }
@@ -71,13 +75,13 @@ class AndroidRepository
         $data = file_get_contents('http://cuti.sidoarjokab.go.id/api_status_cuti.php?nip='.$nip);
         if ($data){
             $pecah = json_decode($data);
-            $back['error'] = 'false';
+            $back['error'] = false;
             $back['code'] = 200;
             $back['message'] = "Berhasil";
             $back['list'] = $pecah;
         }else
         {
-            $back['error'] = 'true';
+            $back['error'] = true;
             $back['code'] = 422;
             $back['message'] = "Data Kosong";
         }
@@ -90,13 +94,13 @@ class AndroidRepository
         $data = file_get_contents('http://cuti.sidoarjokab.go.id/surat_cuti.php?id='.$id);
 
         if ($data){
-            $back['error'] = 'false';
+            $back['error'] = false;
             $back['code'] = 200;
             $back['message'] = "Berhasil";
             $back['data'] =$data;
         }else
         {
-            $back['error'] = 'true';
+            $back['error'] = true;
             $back['code'] = 422;
             $back['message'] = "Data Kosong";
         }
@@ -106,6 +110,72 @@ class AndroidRepository
 
 
     public function upload_photo($request){
-        return 'ok';
+
+        $tujuan_upload = ('public/upload/foto');
+        if($request->hasFile('file')){
+            $file = $request->file('file');
+            $file_extension = $file->getClientOriginalExtension();
+
+
+            $filename = $request->nip.'.png';
+
+            if (file_exists($tujuan_upload.'/'.$filename)) {
+                unlink($tujuan_upload.'/'.$filename);
+            }
+            $file->move($tujuan_upload,$filename);
+        }
+
+        $save = Foto::find($request->nip);
+        $data = file_get_contents($tujuan_upload.'/'.$filename);
+
+        if ($save){
+            $save->Foto1 = $data;
+            $save->save();
+            $back['error'] = false;
+            $back['code'] = 200;
+            $back['message'] = "Data berhasil diubah";
+        }else{
+            $save  = new Foto();
+            $save->Nip = $request->nip;
+            $save->Foto1 = $data;
+            $save->save();
+
+            $back['error'] = false;
+            $back['code'] = 200;
+            $back['message'] = "Data berhasil disimpan";
+        }
+
+
+        return $back;
+
     }
+
+
+    public function save_password($request){
+
+        $data = DB::table('users')
+            ->where('username','like', '%'.$request->nip)
+            ->first();
+
+        if ($data){
+            DB::table('users')
+                ->where('username','like','%'.$request->nip)
+                ->update(['password'=>encrypt($request->password)]);
+
+
+            $back['error'] = false;
+            $back['code'] = 200;
+            $back['message'] = "Berhasil Diubah";
+
+
+        }else{
+            $back['error'] = true;
+            $back['code'] = 422;
+            $back['message'] = "Gagal Diubah";
+        }
+    return $back;;
+    }
+
+
+
 }
